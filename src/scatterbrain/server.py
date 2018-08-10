7995b0f5c84e01c8b199210b4552acc55e506d10
Nate Vack <nate@gimlet.us>
2018-08-09 17:35:41 -0700

    Lots of debugginging

--- src/scatterbrain/server.py
+++ src/scatterbrain/server.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+import flask
+from flask import Flask, render_template, url_for, request
+
+import json
+
+from . import stats
 
 app = Flask(__name__)
 
 
+logger = app.logger
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/regress_voxel')
+def regress_voxel():
+    logger.info(f"I am in regress_voxel, gonna regress {request.args}")
+
+    return json.dumps(request.args)
